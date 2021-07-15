ac790870d140a515b77ecdc077b7e6ea6488e9c1
notnotdee <dlarkinwatson@gmail.com>
2021-07-15 13:36:54 -0700

    basic ui skeleton

--- db/schema/seed.sql
+++ db/schema/seed.sql
@@ -2,6 +2,11 @@ INSERT INTO ingredients (name, category)
 VALUES ('spinach', 'produce')
 RETURNING *;
 
+INSERT INTO ingredients (name, category)
+VALUES ('eggs', 'dairy and eggs')
+RETURNING *;
+
+
 -- default expiry = today + 7 days
 INSERT INTO current_stock (ingredient_id, brand, quantity, unit, purchased_from, price)
 VALUES (1, 'Earthbound Farm', 12, 'oz', 'WinCo', 3.49)
@@ -10,5 +15,5 @@ RETURNING *;
 -- specify an expiry date
 INSERT INTO current_stock 
 (ingredient_id, brand, quantity, unit, purchased_from, price, expiry)
-VALUES (1, 'Earthbound Farm', 16, 'oz', 'WinCo', 4.29, '7/19')
+VALUES (2, 'O Organics', 12, 'ea', 'New Seasons', 4.29, '7/19')
 RETURNING *;
